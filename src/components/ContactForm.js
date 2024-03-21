c22c280d64dfc28d3c7ad87155eeedab2b20c0fe
Janneth Gutierrez <jgutier1@gmail.com>
2024-03-21 14:00:29 -0400

    Exercise: ContactForm - Part 3

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,6 +2,12 @@ import {Button, Label, Col, FormGroup} from 'reactstrap';
 import { Formik, Field, Form } from 'formik';
 
 const ContactForm = () => {
+    const handleSubmit = (values, {resetForm}) => {
+            console.log('form values:', values);
+            console.log('in JSON format:' , JSON.stringify(values));
+            resetForm();
+    };
+
     return (
         <Formik
             initialValues ={{
@@ -14,6 +20,7 @@ const ContactForm = () => {
                 feedback: ''
 
             }}
+            onSubmit={handleSubmit}
             >
 
             <Form>
@@ -99,7 +106,13 @@ const ContactForm = () => {
                         />
                     </Col>
                 </FormGroup>
-                <FormGroup row> </FormGroup>                
+                <FormGroup row> 
+                    <Col md={{size: 10, offset: 2}}>    
+                        <Button type='submit' color='primary'>
+                            Send Feedback
+                        </Button>
+                    </Col>
+                </FormGroup>                
             </Form>
 
         </Formik>
